f22089a9bc9baeb022f7eb678c0db4095f94f143
malasia-415 <55638544+malasia-415@users.noreply.github.com>
2020-03-08 22:12:50 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -5,14 +5,45 @@ import FilterableList from './FilterableList/FilterableList';
 
 
 class App extends Component {
-  render() {
-    return (
-    <div className="App">
-      <SearchBar/>
-      <FilterableList files={this.props.files} />
-    </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            serachTerm: '',
+            filterOption: 'All'
+        };
+     }
+
+updateSearchTerm(term) {
+    this.setState({
+        serachTerm: term 
+    })
 }
+
+updateFileOption(option) {
+    this.setState({
+        fileOption: option 
+    })
+}
+
+
+
+
+render() {
+    return (
+        <div className="App">
+            <SearchBar
+                searchTerm={this.state.searchTerm}
+                filterOption={this.state.filterOption}
+                handleUpdate={term=>this.updateSearchTerm(term)}
+                handleFilterChange={option => this.updateFileOption(option)}/>
+            <FilterableList
+                files={this.props.files}
+                searchTerm={this.state.searchTerm}
+                filterOption={this.state.filterOption}/>
+              </div>
+        );
+    }
+}
+
 
 export default App;
